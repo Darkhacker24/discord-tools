078ceba832c50030b269bb9d941395d9421d3990
Darkhacker24 <93126396+Darkhacker24@users.noreply.github.com>
2021-11-03 00:24:12 +0300

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,9 @@
+@echo off
+pip install colorama
+pip install requests
+pip install datetime
+pip install pyinstaller==4.4
+pip install pyarmor
+pip install dhooks
+pip install chromedriver-autoinstaller
+pip install selenium
